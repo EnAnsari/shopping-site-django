62839692ffab6a5d8d17218eb57aab7211146c16
enansari <rahmat2022a@gmail.com>
2023-09-08 21:46:30 +0330

    fix: start new project

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
