3ee528ac087873e5eb8c09398c7215b6ea90e2a2
enansari <rahmat2022a@gmail.com>
2023-09-09 22:19:33 +0330

    add: adding some models

--- shop/catalogue/models.py
+++ shop/catalogue/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    title = models.CharField(max_length=32, blank=True, null=True)
+
+
+class ProductAttribute(models.Model):
+    title = models.CharField(max_length=32, blank=True, null=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, null=True)
