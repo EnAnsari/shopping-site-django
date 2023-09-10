76a0497e7f4b314a9fe3368c84c8c239871ebcbe
enansari <rahmat2022a@gmail.com>
2023-09-10 17:08:59 +0330

    feat: update views

--- shop/catalogue/urls.py
+++ shop/catalogue/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-
+    path('product/list/', product_list, name='product-list'),
+    path('product/detail/<int:pk>/', product_detail, name='product-detail'),
+    path('category/<int:pk>/products', category_products, name='category-products'),
 ]
--- shop/catalogue/views.py
+++ shop/catalogue/views.py
@@ -1,3 +1,36 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.select_related('category').all()
+    context = "<br>".join([f'{product.title}, {product.upc} - category: {product.category.name}' for product in products])
+    return HttpResponse(context)
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.filter(is_active=True).get(Q(pk=pk) | Q(upc=pk))
+    except Product.DoesNotExist:
+        return HttpResponse('This product doesn\'t exist!')
+    
+    # queryset = Product.objects.filter(is_active=True).filter(Q(pk=pk) | Q(upc=pk))
+    # if queryset.exists():
+    #     product = queryset.first()
+    #     return HttpResponse(f'title: {product.title}')
+    # else:
+    #     return HttpResponse('This product doesn\'t exist!')
+
+    return HttpResponse(f'title: {product.title}')
+
+
+def category_products(request, pk):
+    try:
+        category = Category.objects.prefetch_related('products').get(pk=pk)
+    except Category.DoesNotExist:
+        return HttpResponse('This category doesn\'t exist!')
+    
+    products = category.products.all()
+    context = '<br>'.join(f'{product.title}, {product.upc}' for product in products)
+    return HttpResponse(context)
